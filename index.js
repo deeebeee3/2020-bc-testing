8654482c8a8102ba165359d9c2268fe243ab0f3c
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-06-07 14:36:47 +0100

    Test driven development

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
     forEach(arr, fn){
-        
-        // for(i = 0; i < arr.length; i++){
+
+        // for(let i = 0; i < arr.length; i++){
         //     const value = arr[i];
         //     fn(value, i);
         // }
@@ -9,5 +9,14 @@ module.exports = {
         for (let index in arr){
             fn(arr[index], index);
         }
+    },
+    map(arr, fn){
+        const result = [];
+
+        for(let i = 0; i < arr.length; i++){
+            result.push(fn(arr[i]));
+        }
+
+        return result;
     }
 };
